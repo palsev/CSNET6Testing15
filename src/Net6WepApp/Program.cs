3092d97d5586d604f85129f64f0f12bfe55321c2
palsev <palsev0415@gmail.com>
2023-03-29 11:52:28 -0500

    comm-170275668827752005996772883400570029374

---
+++ src/Net6WepApp/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Commenting out UseSerilog makes test output in xUnit work
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .ReadFrom.Configuration(hostingContext.Configuration);
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Net6WepApp", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Net6WepApp v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Test.Net6WepApp/WeatherForecastTests.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
+using Net6WepApp;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.Net6WepApp
+{
+    public class WeatherForecastTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public WeatherForecastTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task ShouldGetWeatherForecast()
+        {
+            var application = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureLogging(logBuilder =>
+                    {
+                        logBuilder
+                            .SetMinimumLevel(LogLevel.Information)
+                            .ClearProviders()
+                            .AddProvider(new XunitLoggerProvider(_testOutputHelper));
+                    });
+                });
+
+            var client = application.CreateClient();
+
+            var response = await client.GetFromJsonAsync<IEnumerable<WeatherForecast>>("/weatherforecast");
+
+            Assert.NotEmpty(response);
+        }
+    }
+}
